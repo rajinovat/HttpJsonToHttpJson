45c9114558c9ecd3959f739b6ce29900edd45848
rajinovat <rajinovat@gmail.com>
2016-04-25 18:00:57 +1000

    initial commit

---
+++ HttpJsonToHttpJson-Java/src/test/java/com/anz/bl/transform/TransformBLSampleTest.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.anz.bl.transform;
+
+import junit.framework.TestCase;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+import com.anz.common.compute.ComputeInfo;
+
+/**
+ * @author sanketsw
+ *
+ */
+public class TransformBLSampleTest extends TestCase {
+	
+	private static Logger logger = LogManager.getLogger();
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	@Test
+	public void testTransformRequestData() throws Exception {
+		String in  = "{  \"left\": 0,  \"right\": 0 }";
+		String expected = "{  \"right\": 0, \"left\": 100 }";
+		String notExpected = "{  \"left\": 0,  \"right\": 0 }";
+		
+		String out =  new TransformBLSample().execute(in, logger, new ComputeInfo());
+		
+		JSONObject json = new JSONObject(out);
+		JSONAssert.assertEquals(expected, json, false);
+		JSONAssert.assertNotEquals(notExpected, json, false);
+	}
+	
+	
+}
---
+++ HttpJsonToHttpJson-Java/src/test/java/com/anz/bl/transform/TransformBLSampleWithCacheTest.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package com.anz.bl.transform;
+
+import static org.junit.Assert.assertTrue;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+import com.anz.common.compute.ComputeInfo;
+import com.anz.common.dataaccess.models.iib.Operation;
+
+/**
+ * @author sanketsw
+ *
+ */
+public class TransformBLSampleWithCacheTest {
+	
+	private static Logger logger = LogManager.getLogger();
+
+	
+	//@Test
+	public void testTransformResponseData() throws Exception {
+		String in  = "{\"imeplementation\":\"Java_SpringBoot\",\"result\":\"35\"}";
+		String expected = "{\"imeplementation\":\"IIB REST API implementation\"}";
+		String notExpected = "{\"imeplementation\":\"Java_SpringBoot\"}";
+		Operation op = new Operation();	
+		op.setOperation(Operation.ADD);
+		op.setImeplementation("IIB REST API implementation");
+		
+		String out =  new TransformBLSampleWithCache().execute(in, logger, new ComputeInfo());
+		
+		JSONObject json = new JSONObject(out);
+		JSONAssert.assertEquals(expected, json, false);
+		JSONAssert.assertNotEquals(notExpected, json, false);
+	}
+	
+	@Test
+	public void testInvalidResponseDataWithoutResultField() {
+		String in  = "{\"imeplementation\":\"Java_SpringBoot\"}";
+		Operation op = new Operation();	
+		op.setOperation(Operation.ADD);
+		op.setImeplementation("IIB REST API implementation");
+		
+		boolean exceptionThrown =  false;
+		try {
+			String out = new TransformBLSampleWithCache().execute(in, logger, new ComputeInfo());
+		} catch (Exception e) {
+			exceptionThrown = true;
+		}
+		assertTrue(exceptionThrown);
+		
+	}
+	
+	//@Test
+	public void testCachedDataTransformation() throws Exception {
+		String in  = "{\"imeplementation\":\"Java_SpringBoot\",\"result\":\"35\"}";
+		String expected = "{\"operation\":\"Add\"}";
+		
+		Operation op = new Operation();	
+		op.setOperation(Operation.ADD);
+		op.setImeplementation("IIB REST API implementation");
+		String out = null;
+		try {
+			out =  new TransformBLSampleWithCache().execute(in, logger, new ComputeInfo());		
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		JSONObject json = new JSONObject(out);
+		JSONAssert.assertEquals(expected, json, false);
+	}
+
+}
